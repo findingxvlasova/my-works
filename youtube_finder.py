4886230c005cfbbecb6910c2071a3edb5609dc97
Katia <63531419+findingxvlasova@users.noreply.github.com>
2021-03-02 11:29:46 +0200

    Update youtube_finder.py

--- youtube_finder.py
+++ youtube_finder.py
@@ -56,32 +56,5 @@ btn.place(x = 200, y= 150)
 
 root.geometry('400x250')
 root.mainloop()
-'''
-
-print(pg.position())
-screen = int(input("Half screen or all?\n1 - half\n2 - all"))
-find = input("What do u want to watch on youtube? ")
-pg.moveTo(66, 144, 0.5)
-pg.click(66, 144)
-pg.moveTo(342, 86, 0.5)
-pg.click(342, 86)
-pg.typewrite("youtube.com")
-pg.typewrite(["enter"])
-pg.moveTo(668, 203, 0.5)
-time.sleep(2.5)
-pg.click(669, 203)
-pg.typewrite(find)
-pg.typewrite(["enter"])
-time.sleep(2.5)
-pg.moveTo(301, 451, 0.5)
-pg.click(301, 451)
-
-if screen == 1:
-    pg.moveTo(900, 703, 0.5)
-    pg.click(900, 703)
-elif screen == 2:
-    pg.moveTo(1178, 853, 0.5)
-    pg.click(1178, 853)
-'''
 
 
